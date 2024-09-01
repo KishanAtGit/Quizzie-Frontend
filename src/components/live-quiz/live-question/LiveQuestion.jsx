99bee189c90568cdc861aa1dc3f141aea6899418
Uttam <kk82171@gmail.com>
2024-09-01 21:08:04 +0530

    live quiz timer updated

--- src/components/live-quiz/live-question/LiveQuestion.jsx
+++ src/components/live-quiz/live-question/LiveQuestion.jsx
@@ -38,12 +38,15 @@ export default function LiveQuestion({
         <div id='question-numbers'>
           0{currentPage + 1}/0{totalPage}
         </div>
-        <div id='timer'>{timeLeft}</div>
+        {question.timer > 0 && (
+          <div id='timer'>{`00:${
+            timeLeft == 10 ? timeLeft : "0" + timeLeft
+          }s`}</div>
+        )}
       </div>
       <div id='question-text'>{question.questionText}</div>
       <div className='live-quiz-options'>
         {question.options.map((option, index) => {
-          debugger;
           return (
             <div
               className={`${selectedOption === index ? "option-active" : ""}`}
